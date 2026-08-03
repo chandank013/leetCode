83aee91cc9b7ffb66bc2e94788aabe0ba278129a
Chandan Kumar <ck263022@GMAIL.COM>
2026-08-04 00:02:24 +0530

    Time: 0 ms (100%), Space: 9 MB (9.06%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1;
+        int count = 0;
+
+        // skip trailing space
+        while (i >= 0 && s[i] == ' ')
+        {
+            i--;
+        }
+
+        // Count chaactor of last word
+        while ( i >= 0 && s[i] != ' ')
+        {
+            count++;
+            i--;
+        }
+
+        return count;
+    }
+};
