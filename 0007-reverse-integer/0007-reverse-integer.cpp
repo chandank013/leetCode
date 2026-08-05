b8f81e0379e8b9d7672364eb6ea088a68fa80505
Chandan Kumar <ck263022@GMAIL.COM>
2026-08-05 22:16:34 +0530

    Time: 4 ms (20.52%), Space: 8.6 MB (54.87%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -7,15 +7,12 @@ public:
         while(x != 0)
         {
             int rem = x % 10;
-            
-            if (reverse > INT_MAX / 10 || (reverse == INT_MAX/10 && rem > 7))
-                return 0;
-            if (reverse < INT_MIN / 10 || (reverse == INT_MIN/10 && rem < -8))
-                return 0;
-
             reverse = reverse*10 + rem;
             x = x / 10;
         }
+        if (reverse > INT_MAX || reverse < INT_MIN)
+            return 0;
+
         return reverse;
     }
 };
