bd09318ba6e21f23f05cebabecc72f5c29b9aba8
Chandan Kumar <ck263022@GMAIL.COM>
2026-08-11 23:33:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.75%) - LeetHub

---
+++ 0878-nth-magical-number/0878-nth-magical-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        
+        const long long MOD = 1000000007;
+
+        long long lcm = (1LL * a * b) / gcd(a, b);
+
+        long long low = 1;
+        long long high = 1LL * n * min(a, b);
+
+        while (low < high)
+        {
+            long long mid = low + (high - low) / 2;
+
+            long long count =
+                mid / a +
+                mid / b -
+                mid / lcm;
+
+            if (count >= n)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low % MOD;
+
+    }
+};
