269a166f6a43ced1cb927ac65c7b62403a2d895e
Chandan Kumar <ck263022@GMAIL.COM>
2026-08-14 00:11:19 +0530

    Time: 0 ms (100%), Space: 20.6 MB (59.33%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ele = 0;
+
+        for ( int i = 0; i < nums.size(); i++)
+        {
+            ele = ele ^ nums[i];
+        }
+
+        return ele;
+    }
+};
